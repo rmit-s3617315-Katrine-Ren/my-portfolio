b8716c4f48e2d25eab5cc6621f63e4da4eee0e65
rmit-s3617315-Katrine-Ren <renkaixin1991@gmail.com>
2026-05-24 21:49:16 +1000

    Edit main content

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,78 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+    <div className="grid grid-cols-1 gap-12 md:grid-cols-[2fr_1fr]">
+      {/* Left */}
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col gap-13 items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          {/* Intro */}
+          <section>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Portfolio</h1>
+            <p className="text-lg text-stone-500"><p>I’m looking for my next role in <a href="/developer-education-principles"><strong>frontend development</strong></a> or <a href="/dream-role"><strong>e-commerce replatforming</strong></a>. If you’re hiring:</p>
+            </p>
+            <span className="not-prose relative inline-block mt-6">
+              <span className="transition-opacity duration-500 opacity-100"></span>
+              <a href="https://www.linkedin.com/in/katrine-ren-69705a83/" target="_blank" rel="noopener noreferrer"
+                className="relative z-1 inline-flex items-center rounded-md bg-stone-800 px-4 py-2 text-base font-medium text-white no-underline hover:bg-stone-700">Let's talk</a>
+            </span>
+          </section>
+          {/* Work */}
+          <section className="flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
+            <h2 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+              Work
+            </h2>
+            <ul>
+              <li className="text-lg text-stone-500">
+                <a href="https://reebok.com.au/" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Reebok AU</a> Shopify Replatforming | Theme, Storefront, Shoppable Journey, custom JSON templates for drag-n-drop CMS </li>
+              <li className="text-lg text-stone-500">
+                <a href="https://hushpuppies.com.au/" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Hush Puppies</a> Shopify Replatforming | Spearheaded the frontend Shopify migration for Hush Puppies, delivering a high-performance Liquid storefront with custom product layouts, customer account profile, shopping cart and an asynchronous mini-cart to optimize the shopping journey.</li>
+                <li className="text-lg text-stone-500">
+                <a href="https://volley.com/" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Volley</a> Shopify Replatforming Project | Architected the end-to-end Shopify Liquid storefront for the Hush Puppies replatforming, engineering dynamic product pages (PLP/PDP) and a seamless AJAX-driven mini-cart.</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/services/beauty-services" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">David Jones Beauty Service Booking</a> | Enable content to be self-managed in Google Sheets by store service team with modernised UI</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/services/fashion-services" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">David Jones Fashion Service Booking</a>  | Enable content to be self-managed in Google Sheets by store service team with modernised UI</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/careers" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">David Jones Career Hub</a> | also the showcase of the <a href="https://www.davidjones.com/careers" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">custom Cloudinary video player plugin</a> architected and built by me, applying HLS adpative streaming</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/credit-card" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">David Jones Credit Card Hub</a> | Built and maintained</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/blog/fragrance-layering" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Fragrance Layering Blog Post</a> | one of the most popular David Jones blog posts with JuxtaposeJS sliders, ranking in Top 4 Google Search results for keyword "fragrance layering"</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/blog/trends-to-know-in-2026/" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Trends To Know 2026</a> | one of the high-engagment David Jones blog posts on the site and ranked in Top 4 Google Search results for keyword "trends 2026"</li>
+              <li className="text-lg text-stone-500"><a href="https://www.davidjones.com/blog/fragrance-layering" className="font-medium text-zinc-950 dark:text-zinc-50" target="_blank" rel="noopener noreferrer">Year of Horse Horoscope Blog Post</a> | one of the most high-engaging David Jones blog posts with gamified zodiac spinning wheels</li>
+            </ul>
+          </section>
+          {/* Contact */}
+          <section className="flex flex-col gap-4 text-base font-medium sm:flex-row">
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Templates
-            </a>{" "}
-            or the{" "}
+              Deploy Now
+            </a>
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+              Documentation
+            </a>
+          </section>
+        </main>
+      </div>
+      {/* Right sidebar */}
+      <div className="py-36">
+        <h2 className="text-2xl font-semibold text-black dark:text-zinc-50 pb-8">About Me</h2>
+        <p className="text-stone-500">
+          <Image
+            className="float-left mt-0! mr-7 mb-2 w-[3lh] rounded-full"
+            src="/profile.png"
+            alt="Katrine Ren's profile picture"
+            width={400}
+            height={400}
+            priority
+          />
+          Hey, I am Katrine, a Web Developer with 5 years of experience in fashion retail and e-commerce, specialising in Shopify development, frontend performance, and CMS-driven solutions. I bring end-to-end ownership, a get-it-done attitude, and a track record of delivering high-impact digital experiences at scale — from re-platforming retail storefronts to measurable SEO and performance wins.
+        </p>
+      </div>
     </div>
   );
 }
