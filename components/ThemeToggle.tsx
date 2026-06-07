9fb4d1884c58ed57a6f3b8d02673a714a9d987bc
rmit-s3617315-Katrine-Ren <renkaixin1991@gmail.com>
2026-06-07 19:22:36 +1000

    add useSound effect for theme toggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import React from "react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import useSound from 'use-sound';
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
+  const [playOn] = useSound('/sounds/switch-on.mp3', {
+    volume: 0.35,
+  });
+  const [playOff] = useSound('/sounds/switch-off.mp3', {
+    volume: 0.35,
+  });
 
   // Prevent hydration mismatch
   useEffect(() => {
@@ -13,10 +22,21 @@ export default function ThemeToggle() {
   }, []);
 
   if (!mounted) return null;
+  
+
+  const trigger = () => {
+      if (theme === "dark") {
+        playOff();
+      } else {
+        playOn();
+      }
+      setTheme(theme === "dark" ? "light" : "dark");
+    };
+  
 
   return (
     <button
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={trigger}
       className="rounded-md bg-stone-500 p-2 dark:bg-stone-800 transition-colors cursor-pointer"
       aria-label="Toggle theme"
     >
